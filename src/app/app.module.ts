78c8cfb6b5d1e24863ce8b37621a8d5f64b5f852
vivek91-stack <vivekpurohit208@gmail.com>
2021-05-25 13:09:46 +0530

    Updated html to angular With First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    PortfolioComponent,
+    FeedbackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
+import 'zone.assets/js/dist/long-stack-trace-zone';
+import 'zone.assets/js/dist/proxy.js';
+import 'zone.assets/js/dist/sync-test';
+import 'zone.assets/js/dist/jasmine-patch';
+import 'zone.assets/js/dist/async-test';
+import 'zone.assets/js/dist/fake-async-test';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
